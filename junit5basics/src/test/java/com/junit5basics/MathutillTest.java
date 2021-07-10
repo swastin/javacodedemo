b4aa2bcba96b99f0c2405448f9f125bc9f56b95f
swastinuser.email <swastin95.sahoo@gmail.com>
2021-07-10 17:08:44 +0530

    commited by swastin

--- junit5basics/src/test/java/com/junit5basics/MathutillTest.java
+++ junit5basics/src/test/java/com/junit5basics/MathutillTest.java
@@ -1,3 +1,10 @@
+/*************************
+ * 
+ * @author swastin
+ * 
+ *************************/
+
+
 package com.junit5basics;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -6,32 +13,62 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestReporter;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathutillTest {
 	Mathutill mu;
-
+	TestInfo testinfo;
+	TestReporter testReporter;
 	@BeforeEach
-	void init() {
+	void init(TestInfo testinfo,TestReporter testReporter) {
+		
+		this.testinfo=testinfo;
+		this.testReporter=testReporter;
 		mu = new Mathutill();
 
 	}
+	/***********************************
+	 * 
+	 * 
+	 * class for test for addition function
+	 * 
+	 * 
+	 * 
+	 **************************************/
+	
+@Nested
+@Tag("math")
+	class addTest {
 
-	@Test
-	@DisplayName("Add function test")
-	void testAdd() {
+		@Test
+		@DisplayName("Testing add method for +ve")
+		void testAddpositive() {
 
-		int expected = 2;
-		int acttual = mu.add(1, 1);
-		assertEquals(expected, acttual);
+			int expected = 2;
+			int actual = mu.add(1, 1);
+			assertEquals(expected, actual);
+		}
+		@Test
+		@DisplayName("Testing add method for -ve")
+		void testAddnegative() {
+
+			int expected1 = -2;
+			int actual1 = mu.add(-1, -1);
+			assertEquals(expected1, actual1);
+		}
 	}
 
 	@Test
-	@EnabledOnOs(OS.LINUX)
+	//@EnabledOnOs(OS.LINUX)
+	@Tag("math")
 	void testSub() {
 		int expected = 0;
 		int acttual = mu.sub(1, 1);
@@ -40,6 +77,7 @@ class MathutillTest {
 
 //
 	@Test
+	@Tag("math")
 	void testMul() {
 		assumeTrue(true);
 		int expected = 2;
@@ -49,6 +87,7 @@ class MathutillTest {
 
 //
 	@Test
+	@Tag("math")
 	void testDiv() {
 		float expected = (float) 2.0;
 		int acttual = mu.div(2, 1);
@@ -58,17 +97,21 @@ class MathutillTest {
 	}
 
 	@Test
+	@Tag("math")
 	void testMultiply() {
 
 		assertAll(() -> assertEquals(4, mu.mul(2, 2)), () -> assertEquals(0, mu.mul(2, 0)),
 				() -> assertEquals(-4, mu.mul(-2, 2)));
+		
+		
+		System.out.print(testinfo.getDisplayName()+" "+testinfo.getTags()+testinfo.getTestClass());
 
 	}
 
-	@AfterEach
-	void cleanup() {
-
-		System.out.println("Cleaning up");
-	}
+//	@AfterEach
+//	void cleanup() {
+//
+//		System.out.println("Cleaning up");
+//	}
 
 }
